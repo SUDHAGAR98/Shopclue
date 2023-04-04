ef8a33a564c773cd29e0dd46c30883be2f0f963b
SUDHAGAR98 <sudhagar20398@gmail.com>
2023-04-04 20:57:50 +0530

    onebranch

---
+++ src/test/java/org/com/Employee.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Employee {
+
+	private void methodOne() {
+     System.out.println("one");
+	}
+	private void methodTwo() {
+	     System.out.println("two");
+		}
+	
+
+	
+	
+}
